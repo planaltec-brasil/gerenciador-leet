d6ea2432fed9d5dc5259ea6b5548263802376738
Robert W. Cassimiro A. Pinto <44538201+robertcassi@users.noreply.github.com>
2023-06-28 17:22:32 -0300

    Update PdfLeet_controller.php

--- app/Controllers/PdfLeet_controller.php
+++ app/Controllers/PdfLeet_controller.php
@@ -8,39 +8,18 @@ class PdfLeet_controller extends BaseController{
     public function pdfLeet(){
         $pedidos = $_GET['ids'];//implode(',', [ 56,57 ]);
         
-        $temp = explode(',',$pedidos);
-        $qtd = count($temp);
-
         $mpdf = new MeuPDF();
         $data = [
             'pedido' => $this->pdfzinModel->carregaPedidos($pedidos)
         ];
-
-        $post = [
-            'file'=> $data["pedido"][0]->foto_pedido, 
-            'path'=>'uploadLeet',
-            'json' => true
-        ];
-        
-        $ch = curl_init('https://gsplanaltec.com/GerenciamentoServicos/APIControle/s3files');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-        
-        // execute!
-        $response = curl_exec($ch);
-        
-        // close the connection, release resources used
-        curl_close($ch);
-        
-        // do anything you want with your response
-        $linkIMG = json_decode($response);
-        $data['pedido'][0]->link = $linkIMG;
 
         foreach($data['pedido'] as $pedido) {
             $pedido->produtos = $this->pdfzinModel->carregaProdutosPedidos($pedido->id_pedido);
             foreach($pedido->produtos as $produto) {
                 $produto->acrescimos = $this->pdfzinModel->carregaAcrescimosProdutosPedidos($pedido->id_pedido, $produto->produtos_pedido);
             }
+
+            $pedido->foto_pedido = $this->carregaFotosS3($pedido->foto_pedido);
         }
 
 
@@ -71,14 +50,25 @@ class PdfLeet_controller extends BaseController{
 		$mpdf->GerarPDF($html, 'meudoc', $config);
     }
 
-    function fazalgo(){
-        $qtd = count($_POST['ids']);
-        $res = [''];
-        for($i = 0; $i < $qtd; $i++){
-          array_push($res->link,$this->pdfzinModel->carregaPedidos(intval($_POST['ids'][$i])));
-        }
-
-        var_dump($res);
-        exit;
+    function carregaFotosS3($foto_pedido){
+        $post = [
+            'file'=> $foto_pedido, 
+            'path'=>'uploadLeet',
+            'json' => true
+        ];
+        
+        $ch = curl_init('https://gsplanaltec.com/GerenciamentoServicos/APIControle/s3files');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        
+        // execute!
+        $response = curl_exec($ch);
+        
+        // close the connection, release resources used
+        curl_close($ch);
+        
+        // do anything you want with your response
+        $linkIMG = json_decode($response);
+        return $linkIMG;
     }
 }
